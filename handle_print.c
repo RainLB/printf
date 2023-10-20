cda4319834a8e1505719e99452e0657f23938f00
k0uchar <140070212+k0uchar@users.noreply.github.com>
2023-10-20 19:53:28 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * by RainLB
+ * by k0uchar
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
