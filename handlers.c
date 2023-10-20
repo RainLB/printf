fb4d1274f1f7d54844834fc204d8b510de7f15f1
k0uchar <140070212+k0uchar@users.noreply.github.com>
2023-10-20 19:53:55 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -3,7 +3,7 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * by RainLB
+ * by k0uchar
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
