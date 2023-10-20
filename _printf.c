628bbf0a876074691d3b097e7e851c6ebb3a6890
k0uchar <140070212+k0uchar@users.noreply.github.com>
2023-10-20 19:52:14 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format
- * by RainLB
+ * by k0uchar
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
